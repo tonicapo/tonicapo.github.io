730007de0c412605fd6ec3f2d6f0feddfeb53934
ToniCapo <capobianco@Aboulafia.local>
2013-10-17 12:46:44 +0200

    resp

--- Photos.js
+++ Photos.js
@@ -1,33 +1,63 @@
 $("document").ready(function(){
 			var j = 2;
-			var lesImages = ['Railway.jpg', 'HoneyDukes.jpg','Bananas.jpg', 'BW_IMG_6489.jpg','Mail.jpg','chocolatier.jpg '];
+			var lesImages = ['Railway.jpg', 'HoneyDukes.jpg','Bananas.jpg', 'BW_IMG_6489.jpg','chocolatier.jpg '];
 			var image = 2;
 			var promise = null;
+				// alert('gotcha');
 			// $('#photo2').offset({ top: 0, left: ($('#photo1').width()) });		
-		
+
 
 			// var leTiming = new Date();
 
 			var animer = function(){
-				$(".photo").animate({
-					left:"-=100%"
+				    // $(".photo").each(function(){this.style.webkitTransition = '-webkit-translateX 2s ease-out';});
+   					// $(".photo").each(function(){this.style.webkitTransform = '-webkit-translateX(100px)';});
+
+
+					// $('.photo').attr('style', '-webkit-transition: translateX 5s ease-out');
+					// $('.photo').attr('style', '-webkit-transform: translateX(100px)');
+
+					// alert('gotcha');
+						// $(".photo").transition({
+						//   translate: [100,0] ,
+						//   duration: 500,
+						//   easing: 'in',
+						//   complete: function() { /* ... */ }
+						// });
+					// $(".photo").css('-webkit-transition',  "-webkit-translateX 2s ease-out" );
+   					// $(".photo").animate({WebkitTransform:'translateX(200px)'}, 5000);
+
+   					$(".photo").animate({ whyNotToUseANonExistingProperty: 100 }, {
+						    step: function() {
+						        $(this).css('-webkit-transform',"translateX(500px)");
+						        $(this).css('-moz-transform',"translateX(500px)");
+						    },
+						    duration:5000
+						},'ease-out');
+					// $(".photo").css('-webkit-transition',  "-webkit-translateX 2s ease-out" );
+   		// 			$(".photo").css( "-webkit-transform", "translateX(100%)" );
+
+				// $(".photo").animate({
+				// 	left:"-=100%"
 
 					//POur jouer sur l'opacité, on place l'élément initialement invisible à display:none;
 					// opacity:'toggle'
-				}, 1000);
-				setTimeout(changer, 2000)
+				// }, 1000);
+				// setTimeout(changer, 4000);
 				// $(".photo").promise().done(changer());
 				// }, { duration: 2000, done : function(){setTimeout(changer(), 1000);}});
 				// $(".photo").promise().done(changer());
 				// laPhoto2.animate({left:'-=1400'});
 			};
+			setInterval(animer, 1000);
 
 //La fonction est appelée 2 fois!!!
 			function changer(){
 				console.log('changer');
 				if (image==2){
 					$('.1').css('background-image', "url(./photos/"+lesImages[j]+")");
-					$('.1').css('left', '100%');
+
+					// $('.1').css('left', '100%');
 					// $('.1').css('display', 'none');
 
 					// console.log($('.1').css('opacity'));
@@ -35,7 +65,8 @@ $("document").ready(function(){
 					j++;
 				}else{
 					$('.2').css('background-image', "url(./photos/"+lesImages[j]+")");
-					$('.2').css('left', '100%');
+
+					// $('.2').css('left', '100%');
 					image=2;
 					j++;
 				}
@@ -87,5 +118,5 @@ $("document").ready(function(){
 			// 	}
 			// };
 			// setTimeout(function(){$id = setInterval(move, 5);}, 5000);
-			setInterval(animer, 4000);
+
 });

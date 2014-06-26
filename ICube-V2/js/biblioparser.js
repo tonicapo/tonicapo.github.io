6bfd686d6cf015b673708f292767890d5a1c7387
Toni Capo <toni.capo@hotmail.fr>
2014-06-26 16:29:05 +0200

    Json

--- ICube-V2/js/biblioparser.js
+++ ICube-V2/js/biblioparser.js
@@ -3,7 +3,7 @@ $('window').ready(function(){
 //               $(this).toggleClass("btn-primary"); // Switch to default grey
 //               $(this).html("Loading"); // Change text of button
                $.ajax({
-                    url: "../json/biblio.json",
+                    url: "./json/biblio.json",
                     dataType: "jsonp",
                     complete: function(jqXHR, textStatus) {
                         console.log("Completed: "+textStatus);
@@ -15,7 +15,7 @@ $('window').ready(function(){
                         //parse(data);
                         parse(data);
                     }
-//                    jsonpCallback:"parse"
+//                  jsonpCallback:"parse"
                });
 
 //               $(this).html("Load Again"); // Change back text of button

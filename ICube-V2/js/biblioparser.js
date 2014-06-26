64c096b4f69631ff6a49224fd310f6e5360b9412
Toni Capo <toni.capo@hotmail.fr>
2014-06-26 22:56:24 +0200

    Json

--- ICube-V2/js/biblioparser.js
+++ ICube-V2/js/biblioparser.js
@@ -1,46 +1,52 @@
 $('window').ready(function(){
-                var jsonMimeType = "application/json;charset=UTF-8";
+
 //              $("li").remove(); // Remove any existing li elements
 //               $(this).toggleClass("btn-primary"); // Switch to default grey
 //               $(this).html("Loading"); // Change text of button
                $.ajax({
                     url: "./json/biblio.json",
-                    dataType: "jsonp",
-                   contentType: "application/json; charset=utf-8",
-//                    beforeSend: function(x) {
-//                      if(x && x.overrideMimeType) {
-//                       x.overrideMimeType(jsonMimeType);
-//                      }
-//                     },
+                    dataType: "json",
+                    mimeType: "application/json; charset=utf-8",
                     complete: function(jqXHR, textStatus) {
                         console.log("Completed: "+textStatus);
                     },
-//                   complete:function (xhr, status) {
-//                        if (status === 'error' || !xhr.responseText) {
-//                            handleError();
-//                        }
-//                        else {
-//                            var data = xhr.responseText;
-//                            //...
-//                        }
-//                    },
+                   complete:function (xhr, textStatus) {
+                       console.log("Completed: "+textStatus);
+                    },
                     error: function(jqXHR, textStatus, errorThrown) {
                         $("section").html("Error: "+textStatus+" "+errorThrown);
+//                        console.log(responseText);
                     },
                     success: function(data, textStatus, jqXHR) {
-                        //parse(data);
+                        parse(data);
                     },
-                  jsonpCallback:"parse"
+
                });
 
 //               $(this).html("Load Again"); // Change back text of button
 //               $(this).toggleClass("btn-primary"); // Revert back to default grey
-           });
+            });
+
+//Version alternative avec getJSON
+//    $(function() {
+//    $.getJSON("./json/biblio.json",
+//      function(response) {
+//        console.log(response.responseData);
+////        $.each(response.responseData.entries, function (i,newsItem) {
+////            $("#newsfeed").append("<li>"+newsItem.title+"</li>");
+////        });
+//        $("section").append("<h1> Gotcha </h1>");
+//        $("section").append("<p>"+response.articlelist.article.type+"</p>");
+//      });
+//    });
+//
+//     });
 
 function parse(response) {
 //    $.each(response.movies, function (i,movie) {
 //            $("#moviefeed").append("<li>"+movie.title+" ("+movie.runtime+" mins)</li>");
 //    });
-        $(section).append("<h1> Gotcha </h1>");
-        $(section).append("<p>"+response.articlelist.article.type+"</p>");
+        console.log("function call");
+        $("section").append("<h1> Gotcha </h1>");
+        $("section").append("<p>"+response.articlelist.article.type+"</p>");
 }

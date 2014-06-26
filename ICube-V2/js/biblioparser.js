2b9b184a45215b92289c6ffcdd2eb0aeef2be592
Toni Capo <toni.capo@hotmail.fr>
2014-06-26 16:17:04 +0200

    Json

---
+++ ICube-V2/js/biblioparser.js
@@ -0,0 +1,32 @@
+$('window').ready(function(){
+//              $("li").remove(); // Remove any existing li elements
+//               $(this).toggleClass("btn-primary"); // Switch to default grey
+//               $(this).html("Loading"); // Change text of button
+               $.ajax({
+                    url: "../json/,
+                    dataType: "jsonp",
+                    complete: function(jqXHR, textStatus) {
+                        console.log("Completed: "+textStatus);
+                    },
+                    error: function(jqXHR, textStatus, errorThrown) {
+                        $("section").html("Error: "+textStatus+" "+errorThrown);
+                    },
+                    success: function(data, textStatus, jqXHR) {
+                        //parse(data);
+                        $(section).append("<h1> Gotcha </h1>");
+                        $(section).append("<p>"+articlelist.article.type+"</p>";
+
+                    },
+
+//                    jsonpCallback:"parse"
+               });
+
+//               $(this).html("Load Again"); // Change back text of button
+//               $(this).toggleClass("btn-primary"); // Revert back to default grey
+           });
+
+function parse(response) {
+    $.each(response.movies, function (i,movie) {
+            $("#moviefeed").append("<li>"+movie.title+" ("+movie.runtime+" mins)</li>");
+    });
+}
